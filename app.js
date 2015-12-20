6de37d6ae5cac4daf4f070d328353069a426bdd6
lrojas94 <lrojas94@gmail.com>
2015-12-19 23:55:16 -0400

    Done with HandleBars basic training

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var handlebars = require('express-handlebars');
 var port = process.env.PORT || 5000;
 
 app.listen(port, function (error) {
@@ -10,11 +10,14 @@ app.listen(port, function (error) {
 app.use(express.static('public'));
 //Sets:
 app.set('views','src/views');
-app.set('view engine','jade');
+//Set engine
+app.engine('.hbs',handlebars({extname:'.hbs'}));
+app.set('view engine','.hbs');
 //Gets:
 app.get('/', function (request, response) {
     var context = {
-        list : ['a','b']
+        list : ['a','b'],
+        title : 'Handle title :B'
     };
     response.render('index',context);
 });
