b2fde32824c43b8a14ac707a179ffabd6117cede
lrojas94 <lrojas94@gmail.com>
2015-12-20 23:16:43 -0400

    Added controllers handling

---
+++ src/controllers/bookController.js
@@ -0,0 +1,53 @@
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectId;
+var url = 'mongodb://localhost:27017/libraryApp';
+
+var init = function(bookService,nav) {
+
+    var middleware = function(req,res,next) {
+        if (!req.user) {
+            res.redirect('/');
+        }
+        next();
+    };
+
+    var getIndex = function (req, res) {
+        mongodb.connect(url,function(err,db) {
+            var collection = db.collection('books');
+            collection.find({}).toArray(function(err,books) {
+                var context = {
+                    nav : nav,
+                    books : books
+                };
+                res.render('books',context);
+                db.close();
+            });
+        });
+
+    };
+
+    var getById = function(req,res) {
+        mongodb.connect(url,function(err,db) {
+            var collection = db.collection('books');
+            var id = new objectId(req.params.id);
+
+            collection.findOne({_id : id},function(err,book) {
+                var context = {
+                    nav : nav,
+                    books : [book]
+                };
+                res.render('books',context);
+                db.close();
+            });
+        });
+
+    };
+
+    return {
+        middleware : middleware,
+        getIndex : getIndex,
+        getById : getById
+    };
+};
+
+module.exports = init;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -5,46 +5,15 @@ var router = express.Router();
 var url = 'mongodb://localhost:27017/libraryApp';
 
 var init = function(nav) {
+    console.log(require('../controllers/bookController'));
+    var bookController = require('../controllers/bookController')(null,nav);
     //This is how we redirect on the start :D
-    router.use(function(req,res,next) {
-        if (!req.user) {
-            res.redirect('/');
-        }
-        next();
-    });
+    router.use(bookController.middleware);
     router.route('/')
-        .get(function (req, res) {
-            mongodb.connect(url,function(err,db) {
-                var collection = db.collection('books');
-                collection.find({}).toArray(function(err,books) {
-                    var context = {
-                        nav : nav,
-                        books : books
-                    };
-                    res.render('books',context);
-                    db.close();
-                });
-            });
-
-        });
+        .get(bookController.getIndex);
 
     router.route('/:id')
-        .get(function(req,res) {
-            mongodb.connect(url,function(err,db) {
-                var collection = db.collection('books');
-                var id = new objectId(req.params.id);
-
-                collection.findOne({_id : id},function(err,book) {
-                    var context = {
-                        nav : nav,
-                        books : [book]
-                    };
-                    res.render('books',context);
-                    db.close();
-                });
-            });
-
-        });
+        .get(bookController.getById);
 
     return router;
 };
